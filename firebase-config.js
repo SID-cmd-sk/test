68fecd5d0f9bf612b0c413a2c87a7f05f83008cb
SID-cmd-sk <Sidharthkr1859@GMAIL.COM>
2026-05-05 15:05:12 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -10,6 +10,7 @@ window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyDBhIhyjAZlIDBP7bGa4IAiEu2dk07wl5g",
   authDomain: "sidharth-portfolio-789cc.firebaseapp.com",
   projectId: "sidharth-portfolio-789cc",
+  storageBucket: "sidharth-portfolio-789cc.firebasestorage.app",
   messagingSenderId: "288320404329",
   appId: "1:288320404329:web:fd8412870b008a377b1bf8"
 };
